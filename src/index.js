fa05def3ad15520b8f6029f554d7da8b83c43b7c
Kelvin okumba <okumbakelvin@gmail.com>
2023-07-30 22:30:05 +0300

    code challenge

--- src/index.js
+++ src/index.js
@@ -1,87 +1,137 @@
-// code here
+const init = () => {
+
+  fetch('http://localhost:3000/beers/1')
+      .then(response => response.json())
+      .then(data => {
+        let beerName = document.querySelector('#beer-name');
+        let beerDescription = document.querySelector('#beer-description');
+
+        document.getElementById("beer-image").src=data.image_url;
+
+        beerName.innerText = data.name;
+        beerDescription.innerText = data.description;
+        
+
+        let reviewList = document.createElement("ul");
+        reviewList.setAttribute('id','review-list')
+  
+        for (review in data.reviews){
+        let reviewText = document.createElement("li");
+        reviewText.innerText = data.reviews[review];
+        reviewList.appendChild(reviewText);     
+        }
+  
+        document.getElementById('review-list').parentNode.replaceChild(reviewList,document.getElementById('review-list'));
+         
+      }
+      )
 
 
-//variables
-const beerMenu = document.getElementById('beer-list');
-const beerDetails = document.querySelector('.beer-details');
-const reviewForm = document.getElementById('review-form');
-const reviewInput = document.getElementById('review');
+      fetch('http://localhost:3000/beers')
+      .then(response => response.json())
+      .then(data => {
+  
+          let beerList = document.createElement("ul");
+          
+          beerList.setAttribute('id','beerList');
+  
+          for (beer in data){
+              let beerName = document.createElement("li");
+              beerName.innerText = data[beer].name;
+              beerList.appendChild(beerName);     
+              }
+  
+  
+      document.getElementById('beer-list').parentNode.replaceChild(beerList,document.getElementById('beer-list'));
+  
+      });
+  
+  
+      }; 
 
-// variable name for base URL for the API
-const apiUrl = 'http://localhost:3000';
 
-// Function to render the beer menu in the <nav> element
-function renderBeerMenu(beers) {
-  beerMenu.innerHTML = '';
-  beers.forEach((beer) => {
-    const menuItem = document.createElement('li');
-    menuItem.textContent = beer.name;
-    menuItem.addEventListener('click', () => displayBeerDetails(beer));
-    beerMenu.appendChild(menuItem);
-  });
-}
+document.addEventListener('DOMContentLoaded', init); //we populate the web page on refresh
+  
 
-// Function to display beer details in the main section
-function displayBeerDetails(beer) {
-  beerDetails.innerHTML = `
-    <h2 id="beer-name">${beer.name}</h2>
-    <img id="beer-image" src="${beer.image_url}" alt="${beer.name}" width="200" height="200" />
-    <p><em id="beer-description">${beer.description}</em></p>
-    <h3>Customer Reviews</h3>
-    <ul id="review-list">
-      ${beer.reviews.map((review) => `<li>${review}</li>`).join('')}
-    </ul>
-    <form id="review-form">
-      <label for="review">Your Review:</label>
-      <textarea id="review"></textarea>
-      <button type="submit">Add review</button>
-    </form>
-  `;
-}
+// Updating the  beer description (both in the server and in the webpage)
 
-// Function to add a new review to the selected beer
-function addReview(beerId, review) {
-  // Send the new review to the server (no persistence, just for display purposes)
-  fetch(`${apiUrl}/beers/${beerId}`, {
+const inputForm1 = document.querySelector('button');
+inputForm1.addEventListener('click', (event) => {
+
+  event.preventDefault();
+
+  // updating the server
+
+  const description = document.querySelector('#description').value;
+
+  let beerDescription = document.querySelector('#beer-description');
+  beerDescription.innerText = description;
+
+  let descriptionArray = {
+    "description":description
+  };
+
+  // Updating the server
+
+  fetch('http://localhost:3000/beers/1',{
     method: 'PATCH',
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type":"application/json"
     },
-    body: JSON.stringify({
-      reviews: [review], // Replace existing reviews with the new one
-    }),
+    body:JSON.stringify(descriptionArray)
+  }
+).then((r) => r.json)
+.then((description) => console.log(description))
+.catch((e) => console.log(e))
+
   });
 
-  // show the new review (again, no persistence, just for display)
-  const newReview = document.createElement('li');
-  newReview.textContent = review;
-  document.getElementById('review-list').appendChild(newReview);
+// Updating the  reviews (both in the server and in the webpage)
+
+const inputForm2 = document.querySelector('#review-form').querySelector('button');
+inputForm2.addEventListener('click', (event) => {
+  
+      event.preventDefault();
+
+      fetch('http://localhost:3000/beers/1')
+      .then(response => response.json())
+      .then(data => {
+  
+        let reviews = data.reviews;
+    
+
+      let review = document.querySelector('#review').value;   // my review as a string
+
+      reviews.push(review);
+      
+    // Updating the DOM
+
+      
+      let node = document.createElement("li");
+      let textnode = document.createTextNode(review);
+      node.appendChild(textnode);
+      document.getElementById("review-list").appendChild(node);
+      
+
+
+      // Updating the server
+
+  
+      fetch('http://localhost:3000/beers/1',{
+        method: 'PATCH',
+        headers: {
+          "Content-Type":"application/json"
+        },
+        body:JSON.stringify(
+          {
+          reviews:reviews
+      })
+      }
+  ).then((r) => r.json)
+  .then((review) => console.log(review))
+  .catch((e) => console.log(e))
+  
 }
+) 
 
-// Event listener for the review form submission
-reviewForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const reviewText = reviewInput.value.trim();
-
-  if (reviewText !== '') {
-    const selectedBeerId = getCurrentDisplayedBeerId(); // function to get the current displayed beer ID
-    if (selectedBeerId) {
-      addReview(selectedBeerId, reviewText);
-    }
-    reviewInput.value = '';
-  }
-});
-// Make a GET request to retrieve all the beers
-fetch(`${apiUrl}/beers`)
-  .then((response) => response.json())
-  .then((data) => {
-    renderBeerMenu(data); // Render the beer menu
-    if (data.length > 0) {
-      // Display the details of the first beer initially
-      fetch(`${apiUrl}/beers/${data[0].id}`)
-        .then((response) => response.json())
-        .then((firstBeer) => displayBeerDetails(firstBeer));
-    }
-  })
-  .catch((error) => console.error('Error fetching beer data:', error));
-
+      });
